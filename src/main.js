5c50350c0b0ba072a10f92b54bfa018a65f12e10
lw <760548616@qq.com>
2024-11-28 15:51:36 +0800

    添加切换语言功能

--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
-/*
- * @Author: hiddenSharp429 z404878860@163.com
- * @Date: 2024-10-28 19:44:55
- * @LastEditors: hiddenSharp429 z404878860@163.com
- * @LastEditTime: 2024-10-28 21:37:58
- */
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { createI18n } from 'vue-i18n' // Use vue-i18n for Vue 3
 import { API_BASE_URL } from '@/config'
+import en from './locales/en.json'
+import zh from './locales/zh.json'
 
-// 配置 axios 默认值
+// Configure axios defaults
 axios.defaults.baseURL = API_BASE_URL
 const token = localStorage.getItem('token')
 if (token) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
+// Create i18n instance
+const i18n = createI18n({
+  legacy: false, // Use Composition API
+  locale: 'zh',
+  messages: {
+    en,
+    zh
+  }
+})
+
+// Create Vue app
 const app = createApp(App)
 app.use(router)
+app.use(i18n)
 app.mount('#app')
